343ddc9e76f4c6dead0159240f80e47415a03b39
Jeff Hsueh <JeffHsueh@users.noreply.github.com>
2015-10-22 11:52:53 +0800

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -8,11 +8,7 @@ class Solution(object):
         """
         if len(nums) ==0:
             return 0
-        elif len(nums) ==1:
-            if 0 not in nums:
-                return 0
-            elif 1 not in nums:
-                return 1
+        
         else:
             
             ls = [i for i in range(len(nums)+1)]
